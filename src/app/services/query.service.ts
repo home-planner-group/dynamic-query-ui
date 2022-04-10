f75b895ef56885b7354b76b09496f730da140667
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2022-04-10 20:48:29 +0200

    implemented error handling

--- src/app/services/query.service.ts
+++ src/app/services/query.service.ts
@@ -1,9 +1,11 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../environments/environment";
-import {Observable} from "rxjs";
+import {catchError, Observable, of} from "rxjs";
 import {QueryResponse} from "../models/QueryResponse";
 import {QueryRequest} from "../models/QueryRequest";
+import {OpenWarnSnackBar} from "../utility/snackbar";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: 'root'
@@ -15,21 +17,71 @@ export class QueryService {
     headers: new HttpHeaders({'Content-Type': 'application/json'})
   };
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient,
+              private snackBar: MatSnackBar) {
+  }
+
+  private static buildErrorMessage(error: HttpErrorResponse) {
+    let errorMessage: string = '';
+    // exception prefix
+    if (error.error.exception) {
+      errorMessage += error.error.exception + ': '
+    }
+    // message
+    if (error.error.message) {
+      errorMessage += error.error.message;
+    } else {
+      errorMessage += 'No message found.'
+    }
+    // status part
+    if (error.status === 0) {
+      errorMessage += ' [API not available!]'
+    } else {
+      errorMessage += ' [' + error.statusText + ']';
+    }
+    return errorMessage;
+  }
+
+  private static buildErrorResponse(errorMessage: string): QueryResponse {
+    return {
+      instanceCount: 0,
+      columnDef: [],
+      rows: [],
+      errorCount: 1,
+      errorMessages: [
+        errorMessage
+      ]
+    }
   }
 
   public dynamicQuery(queryModel: QueryRequest): Observable<QueryResponse> {
     const endpointUrl = this.controllerUrl + '/dynamic';
-    return this.httpClient.post<QueryResponse>(endpointUrl, queryModel, this.defaultHeaders);
+    return this.httpClient.post<QueryResponse>(endpointUrl, queryModel, this.defaultHeaders).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error(error);
+        return of(QueryService.buildErrorResponse(QueryService.buildErrorMessage(error)));
+      })
+    );
   }
 
   public staticQuery(fileName: string): Observable<QueryResponse> {
     const endpointUrl = this.controllerUrl + '/static?file=' + fileName;
-    return this.httpClient.post<QueryResponse>(endpointUrl, fileName, this.defaultHeaders);
+    return this.httpClient.post<QueryResponse>(endpointUrl, fileName, this.defaultHeaders).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error(error);
+        return of(QueryService.buildErrorResponse(QueryService.buildErrorMessage(error)));
+      })
+    );
   }
 
   public getFiles(): Observable<string[]> {
     const endpointUrl = this.controllerUrl + '/static-files';
-    return this.httpClient.get<string[]>(endpointUrl, this.defaultHeaders);
+    return this.httpClient.get<string[]>(endpointUrl, this.defaultHeaders).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error(error);
+        OpenWarnSnackBar(this.snackBar, QueryService.buildErrorMessage(error));
+        return of([]);
+      })
+    );
   }
 }
